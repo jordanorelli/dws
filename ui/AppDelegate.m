4cdfa60f8e5564d3e622f92ed9377d975fba6999
Jordan Orelli <jordanorelli@gmail.com>
2017-06-11 19:34:01 -0500

    fix whitespace lol

--- ui/AppDelegate.m
+++ ui/AppDelegate.m
@@ -5,13 +5,13 @@
 // Application Startup ------------------------------------------------------{{{
 
 - (void) applicationWillFinishLaunching:(NSNotification *)notification {
-	NSLog(@"applicationWillFinishLaunching notification: %@", notification);
-	NSLog(@"Main Menu in applicationWillFinishLaunching: %@", [NSApp mainMenu]);
+    NSLog(@"applicationWillFinishLaunching notification: %@", notification);
+    NSLog(@"Main Menu in applicationWillFinishLaunching: %@", [NSApp mainMenu]);
 }
 
 - (void) applicationDidFinishLaunching:(NSNotification *)notification {
-	NSLog(@"applicationDidFinishLaunching notification: %@", notification);
-	NSLog(@"Main Menu in applicationDidFinishLaunching: %@", [NSApp mainMenu]);
+    NSLog(@"applicationDidFinishLaunching notification: %@", notification);
+    NSLog(@"Main Menu in applicationDidFinishLaunching: %@", [NSApp mainMenu]);
     [NSApp activateIgnoringOtherApps:YES];
 }
 
@@ -19,91 +19,91 @@
 // Application Termination --------------------------------------------------{{{
 
 - (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender {
-	NSLog(@"applicationShouldTerminate sender: %@", sender);
-	return NSTerminateNow;
+    NSLog(@"applicationShouldTerminate sender: %@", sender);
+    return NSTerminateNow;
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
-	NSLog(@"applicationShouldTerminateAfterLastWindowClosed sender: %@", sender);
-	return YES;
+    NSLog(@"applicationShouldTerminateAfterLastWindowClosed sender: %@", sender);
+    return YES;
 }
 
 - (void)applicationWillTerminate:(NSNotification *)notification {
-	NSLog(@"applicationWillTerminate notification sender: %@", notification);
+    NSLog(@"applicationWillTerminate notification sender: %@", notification);
 }
 
 // --------------------------------------------------------------------------}}}
 // Application Active Status ------------------------------------------------{{{
 
 - (void)applicationWillBecomeActive:(NSNotification *)notification {
-	NSLog(@"applicationWillBecomeActive notification: %@", notification);
+    NSLog(@"applicationWillBecomeActive notification: %@", notification);
 }
 
 - (void)applicationDidBecomeActive:(NSNotification *)notification {
-	NSLog(@"applicationDidBecomeActive notification: %@", notification);
+    NSLog(@"applicationDidBecomeActive notification: %@", notification);
 }
 
 - (void)applicationWillResignActive:(NSNotification *)notification {
-	NSLog(@"applicationWillResignActive notification: %@", notification);
+    NSLog(@"applicationWillResignActive notification: %@", notification);
 }
 
 - (void)applicationDidResignActive:(NSNotification *)notification {
-	NSLog(@"applicationDidResignActive notification: %@", notification);
+    NSLog(@"applicationDidResignActive notification: %@", notification);
 }
 
 // --------------------------------------------------------------------------}}}
 // Application Hide Status --------------------------------------------------{{{
 
 - (void)applicationWillHide:(NSNotification *)notification {
-	NSLog(@"applicationWillHide notification: %@", notification);
+    NSLog(@"applicationWillHide notification: %@", notification);
 }
 
 - (void)applicationDidHide:(NSNotification *)notification {
-	NSLog(@"applicationDidHide notification: %@", notification);
+    NSLog(@"applicationDidHide notification: %@", notification);
 }
 
 - (void)applicationWillUnhide:(NSNotification *)notification {
-	NSLog(@"applicationWillUnhide notification: %@", notification);
+    NSLog(@"applicationWillUnhide notification: %@", notification);
 }
 
 - (void)applicationDidUnhide:(NSNotification *)notification {
-	NSLog(@"applicationDidUnhide notification: %@", notification);
+    NSLog(@"applicationDidUnhide notification: %@", notification);
 }
 
 // --------------------------------------------------------------------------}}}
 // Application Update Status ------------------------------------------------{{{
 
 - (void)applicationWillUpdate:(NSNotification *)notification {
-	// NSLog(@"applicationWillUpdate notification: %@", notification);
+    // NSLog(@"applicationWillUpdate notification: %@", notification);
 }
 
 - (void)applicationDidUpdate:(NSNotification *)notification {
-	// NSLog(@"applicationDidUpdate notification: %@", notification);
+    // NSLog(@"applicationDidUpdate notification: %@", notification);
 }
 
 // --------------------------------------------------------------------------}}}
 // Menu Bar -----------------------------------------------------------------{{{
 
 - (void) createMenuBar {
-	NSLog(@"Will create menu bar here");
-	NSLog(@"creating menu bar. initial main menu bar: %@", [NSApp mainMenu]);
+    NSLog(@"Will create menu bar here");
+    NSLog(@"creating menu bar. initial main menu bar: %@", [NSApp mainMenu]);
 
-	id mainMenu = [NSMenu alloc];
-	[mainMenu initWithTitle:@"Main Menu"];
-	[NSApp setMainMenu:mainMenu];
+    id mainMenu = [NSMenu alloc];
+    [mainMenu initWithTitle:@"Main Menu"];
+    [NSApp setMainMenu:mainMenu];
 
-	id appMenuItem = [NSMenuItem alloc];
-	[appMenuItem initWithTitle:@"dws" action:NULL keyEquivalent:@""];
-	[mainMenu addItem:appMenuItem];
+    id appMenuItem = [NSMenuItem alloc];
+    [appMenuItem initWithTitle:@"dws" action:NULL keyEquivalent:@""];
+    [mainMenu addItem:appMenuItem];
 
-	id appMenu = [NSMenu alloc];
-	[appMenu initWithTitle:@"dws"];
-	[appMenuItem setSubmenu:appMenu];
+    id appMenu = [NSMenu alloc];
+    [appMenu initWithTitle:@"dws"];
+    [appMenuItem setSubmenu:appMenu];
 
-	id quitMenuItem = [NSMenuItem alloc];
-	[quitMenuItem initWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
-	[appMenu addItem:quitMenuItem];
-	NSLog(@"assigned to main menu: %@", [NSApp mainMenu]);
+    id quitMenuItem = [NSMenuItem alloc];
+    [quitMenuItem initWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@"q"];
+    [appMenu addItem:quitMenuItem];
+    NSLog(@"assigned to main menu: %@", [NSApp mainMenu]);
 }
 
 // --------------------------------------------------------------------------}}}
--- ui/ui_darwin.m
+++ ui/ui_darwin.m
@@ -6,25 +6,25 @@ id appDelegate;
 
 void Initialize(void) {
     NSLog(@"Initializing with processInfo: %@", [[NSProcessInfo processInfo] arguments]);
-	NSLog(@"Creating Autorelease Pool");
+    NSLog(@"Creating Autorelease Pool");
     defaultAutoreleasePool = [NSAutoreleasePool new];
     [NSApplication sharedApplication];
-	NSLog(@"Setting App Delegate");
-	appDelegate = [AppDelegate new];
-	[appDelegate createMenuBar];
-	[NSApp setDelegate: appDelegate];
+    NSLog(@"Setting App Delegate");
+    appDelegate = [AppDelegate new];
+    [appDelegate createMenuBar];
+    [NSApp setDelegate: appDelegate];
     [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
-	NSLog(@"Initialization complete");
+    NSLog(@"Initialization complete");
 }
 
 
 int Run(void) {
-	NSLog(@"Entered Run");
-	NSLog(@"Activating App");
-	NSLog(@"Running App Event Loop");
+    NSLog(@"Entered Run");
+    NSLog(@"Activating App");
+    NSLog(@"Running App Event Loop");
     [NSApp run];
-	NSLog(@"App Event Loop finished. Draining pool.");
-	[defaultAutoreleasePool drain];
-	NSLog(@"Leaving Run");
+    NSLog(@"App Event Loop finished. Draining pool.");
+    [defaultAutoreleasePool drain];
+    NSLog(@"Leaving Run");
     return 0;
 }
